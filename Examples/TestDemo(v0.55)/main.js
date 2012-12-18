8b46ae8d90e0f0b07de8ea8955773de3a99232b3
Mattia Russomando <mattia.russomando@gmail.com>
2012-12-18 15:10:31 +0100

    updates

--- Examples/TestDemo(v0.55)/main.js
+++ Examples/TestDemo(v0.55)/main.js
@@ -44,7 +44,7 @@ function TestDemo(params)
         ghostTexture = MyCanvas.loadTexture("Assets/ghost.png", function () { bgPattern = MyCanvas.getPattern(bgTexture, 'repeat'); }); 
         bgTexture = MyCanvas.loadTexture("Assets/texture.png", null);
         //audio
-        audioTest = MyCanvas.loadAudio("Assets/audio/audioDemo.m4a", false, false);      
+        audioTest = MyCanvas.loadAudio("Assets/audio/audioDemo.m4a", true, false);      
         audioTest.volume = 1.0;
         audioTest.loop = true;
         //gradient
